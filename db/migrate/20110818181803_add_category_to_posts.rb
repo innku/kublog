43595f7d6453f7db9da018f110734da86e009d31
Adrian Cuadros <adrian@innku.com>
2011-08-24 06:17:36 -0500

    Removes all dummy public uploads

--- db/migrate/20110818181803_add_category_to_posts.rb
+++
@@ -1,8 +0,0 @@
-class AddCategoryToPosts < ActiveRecord::Migration
-  def change
-    change_table :kublog_posts do |t|
-      t.references :category
-    end
-    add_index :kublog_posts, :category_id
-  end
-end
